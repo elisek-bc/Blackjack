097f6e8b7a9a43a89b6389defb85bb12fb8be438
elisek-bc <elisekppns@gmail.com>
2019-05-24 11:53:41 +0200

    start blackjack, stopped blackjack due to unconvenient and unsufficient info

---
+++ src/home.php
@@ -0,0 +1,15 @@
+<?php
+
+Class Blackjack {
+    public function hit() {
+        $amount .= rand(1,11)
+    }
+
+    public function stand() {
+        
+    }
+
+    public function surrender(){
+
+    }
+}
